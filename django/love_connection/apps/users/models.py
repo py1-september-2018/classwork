8d72cf05b45a3135f87bca30d9e5d80b5d51c262
Weston Harper <wes@tao.team>
2018-10-10 19:04:21 -0600

    added registration

--- django/love_connection/apps/users/models.py
+++ django/love_connection/apps/users/models.py
@@ -1,15 +1,61 @@
 from django.db import models
-
+import re
+import bcrypt
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 # Create your models here.
 class UserManager(models.Manager):
-  pass
+  def validate(self, form):
+    errors = []
+
+    if len(form['first_name']) < 3:
+      errors.append('First name must be at least 3 characters long')
+    if len(form['last_name']) < 3:
+      errors.append('Last name must be at least 3 characters long')
+    if not EMAIL_REGEX.match(form['email']):
+      errors.append('Email must be valid')
+    if len(form['password']) < 8:
+      errors.append('Password must be at least 8 characters long')
+    if not 'gender' in form:
+      errors.append('Must select a gender')
+    if len(form['description']) < 3:
+      errors.append('Description must be at least 3 characters long')
+    if len(form['description']) > 255:
+      errors.append('Description must not exceed 255 characters')
+    if form['birth_date'] == "":
+      errors.append('Must select a birth date')
+
+    # self.filter(email=form['email'])
+    try:
+      self.get(email=form['email'])
+      errors.append('Email already in use')
+    except:
+      pass
+    
+    return errors
+
+  def create_user(self, user_data):
+    # hash the user's password
+    pw_hash = bcrypt.hashpw(user_data['password'].encode(), bcrypt.gensalt())
+    # create user
+    user = self.create(
+      first_name=user_data['first_name'],
+      last_name=user_data['last_name'],
+      email=user_data['email'],
+      gender=user_data['gender'],
+      birth_date=user_data['birth_date'],
+      pw_hash=pw_hash
+    )
+    return user
 
 class User(models.Model):
   first_name = models.CharField(max_length=255)
   last_name = models.CharField(max_length=255)
+  email = models.CharField(max_length=255)
   gender = models.CharField(max_length=255)
   pw_hash = models.CharField(max_length=500)
   description = models.CharField(max_length=255)
   birth_date = models.DateTimeField()
+  matches = models.ManyToManyField("self")
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
+  objects = UserManager()
--- django/love_connection/apps/users/views.py
+++ django/love_connection/apps/users/views.py
@@ -1,11 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import User
 
 # Create your views here.
 def index(req):
   pass
 
 def create(req):
-  pass
+  if req.method != 'POST':
+    return redirect('users:new')
+
+  errors = User.objects.validate(req.POST)
+  if len(errors) > 0:
+    for error in errors:
+      messages.error(req, error)
+  else:
+    user = User.objects.create_user(req.POST)
+    print(user)
+    req.session['user_id'] = user.id
+  return redirect('users:new')
 
 def update(req, id):
   pass
@@ -14,7 +27,7 @@ def delete(req, id):
   pass
 
 def new(req):
-  pass
+  return render(req, 'users/new.html')
 
 def edit(req, id):
   pass
